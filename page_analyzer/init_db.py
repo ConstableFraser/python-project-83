b4932af5c4ad2b86fca8731ae8a878aaf39f514d
sanswed <sanswed@gmail.com>
2023-03-10 03:07:00 +0300

    fix bug

--- page_analyzer/init_db.py
+++ page_analyzer/init_db.py
@@ -11,6 +11,12 @@ host = os.getenv("HOST")
 
 
 def get_db():
+    load_dotenv()
+    dbname = os.getenv("DBNAME")
+    user = os.getenv("USER")
+    password = os.getenv("PASSWORD")
+    host = os.getenv("HOST")
+
     print("DBNAME:\t", dbname)
     print("USER:\t", user)
     return psycopg2.connect(dbname=dbname,
