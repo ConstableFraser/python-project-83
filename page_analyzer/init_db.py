6356ebd24a50708f06a69a46f7981e0f017b69e0
sanswed <sanswed@gmail.com>
2023-03-18 17:17:02 +0300

    fix review#3

--- page_analyzer/init_db.py
+++ page_analyzer/init_db.py
@@ -13,5 +13,19 @@ def get_db():
 def init_db():
     with get_db() as conn:
         with conn.cursor() as cur:
-            cur.execute(open("database.sql", "r").read())
+            # cur.execute(open("database.sql", "r").read())
+            cur.execute("DROP TABLE IF EXISTS url_checks;")
+            cur.execute("DROP TABLE IF EXISTS urls;")
+            cur.execute("CREATE TABLE urls( \
+                        id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, \
+                        name varchar(255) UNIQUE NOT NULL, \
+                        created_at float);")
+            cur.execute("CREATE TABLE url_checks( \
+                        id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, \
+                        url_id bigint REFERENCES urls (id), \
+                        status_code integer, \
+                        h1 text, \
+                        title varchar(255), \
+                        description text, \
+                        created_at float);")
             conn.commit()
